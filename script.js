b86a48ec547574e948aa0d3b67858bc89fc5b2e8
DMalik214 <163868959+DMalik214@users.noreply.github.com>
2024-03-29 16:26:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive navbar toggle
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
